9ed304cf3272f8087a4530990ab632318ba7156a
Danilo Policarpo <danilo.policarpo@concrete.com.br>
2017-09-22 18:06:19 -0300

    atualiza projeto

--- features/ios/screens/acessar_site_screen.rb
+++ features/ios/screens/acessar_site_screen.rb
@@ -2,21 +2,20 @@ class AcessarSiteScreen
   attr_accessor :driver
   def initialize(driver)
     @driver = driver
-    @layout_name =  'XCUIElementTypeButton'
+    @layout_name = 'XCUIElementTypeButton'
     @url = 'XCUIElementTypeTextField'
     @enter = 'XCUIElementTypeButton'
     @home_site = 'XCUIElementTypeLink'
-
   end
 
   def acessar_homePage
-    @driver.find_element(class:"#{@layout_name}").text
-  end  
+    @driver.find_element(class: @layout_name.to_s).text
+  end
 
   def acessar_site(site)
-    @site = CREDENTIALS[site.gsub(' ', '_').to_sym][:url]    
-    @driver.find_element(class: "#{@url}").send_keys @site
-    @driver.find_element(class: "#{@enter}").click
+    @site = CREDENTIALS[site.tr(' ', '_').to_sym][:url]
+    @driver.find_element(class: @url.to_s).send_keys @site
+    @driver.find_element(class: @enter.to_s).click
   end
 
   def visualizar_home_site
@@ -24,7 +23,6 @@ class AcessarSiteScreen
     # wait = Selenium::WebDriver::Wait.new(:timeout => 20)
     # fail "Site não aberto" if wait.until{ @driver.find_element(class: "#{@home_site}").displayed? != true}
 
-    fail "Site não aberto" if (@driver.find_element(class: "#{@home_site}").displayed? != true)
-
+    raise 'Site não aberto' if @driver.find_element(class: @home_site.to_s).displayed? != true
   end
 end
