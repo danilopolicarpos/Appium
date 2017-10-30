e9913980b20cfb20629fabd0252037d0db8d46e7
wanderley Landim <wanderleylandim@wanderley-mito.local>
2017-10-30 17:49:12 -0200

    ajuste

--- features/ios/screens/acessar_site_screen.rb
+++ features/ios/screens/acessar_site_screen.rb
@@ -12,7 +12,7 @@ class AcessarSiteScreen
 
   def acessar_site(site)
     @site = CREDENTIALS[site.tr(' ', '_').to_sym][:url]
-    find_element(class: @url.send_keys @site
+    find_element(class: @url).send_keys @site
     find_element(class: @enter).click
   end
 
