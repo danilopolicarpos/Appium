699d268166b0730f7d2b7f61013617854dfb8215
Deyvirson Mendonça <deyvirson.mendonca@concrete.com.br>
2018-05-04 15:56:30 -0300

    ädiçao After @reinstall

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,6 +6,12 @@ After do
   $driver.driver_quit
 end
 
-Before ('@reinstall') do 
+Before ('@reinstall') do
   reinstall_apps
+end
+
+After ('@reinstall') do
+  @caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'ios_caps.txt')
+  Appium::Driver.new(@caps, true)
+  Appium.promote_appium_methods Object
 end
