6b5b437cd6d9213282fd3b9ad90631fae4c9fea9
Policarpo <danilo.policarpo@accenture.com>
2018-08-27 13:17:32 -0300

    ajuste

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,20 @@
 require 'rspec/expectations'
 require 'appium_lib'
 require 'pry'
+require 'parallel'
 
 if ENV['PLATFORM'] == 'ios'
   caps = Appium.load_appium_txt file: File.expand_path('./../../../ios_appium.txt', __FILE__), verbose: true
 elsif ENV['PLATFORM'] == 'android'
-  caps = Appium.load_settings file: File.join('./caps_android', 'oreo.txt'), verbose: true
+    binding.pry
+    files = Dir['./caps_android/*']
+    resultado = Hash[files.collect { |item| [item.gsub(" ", "")] } ]
+
+    resultado = Parallel.map(resul) do |x|
+     caps = Appium.load_settings file: File.join(x), verbose: true
+     puts caps
+    end
+  #caps = Appium.load_settings file: File.join('./caps_android', 'oreo.txt'), verbose: true
 end
 
 Appium::Driver.new(caps, true)
