622763e644bb9af89b8fad23abee51498181bf1e
Policarpo <danilo.policarpo@accenture.com>
2018-09-11 18:11:29 -0300

    rubcop ajuste

--- features/android/android_screen_base.rb
+++ features/android/android_screen_base.rb
@@ -1,33 +1,35 @@
+# frozen_string_literal: true
+
 # wait_for_element(id: @contato,:timeout => 5)
 def wait_for_element(atributos)
-    begin
-        wait = Selenium::WebDriver::Wait.new 
-        wait.until { find_element(atributos).displayed? }
-    rescue Selenium::WebDriver::Error::TimeOutError => e
-      raise "Não encontrou resultado da busca \n #{e.message}"
-    end
+  begin
+  wait = Selenium::WebDriver::Wait.new
+  wait.until { find_element(atributos).displayed? }
+  rescue Selenium::WebDriver::Error::TimeOutError => e
+    raise "Não encontrou resultado da busca \n #{e.message}"
+end
 end
 
 # wait_for_click(:id 'xpto')
 def wait_for_click(atributos)
-    begin
-        wait = Selenium::WebDriver::Wait.new :timeout => 5
-        wait { find_element(atributos).click }
-    rescue Selenium::WebDriver::Error::TimeOutError => e
-        raise "Não consegui efetuar a ação \n #{e.message}"
-    end
+  begin
+    wait = Selenium::WebDriver::Wait.new
+    wait { find_element(atributos).click }
+  rescue Selenium::WebDriver::Error::TimeOutError => e
+    raise "Não consegui efetuar a ação \n #{e.message}"
+  end
 end
 
 def reinstall_apps
-@driver.reset
+  @driver.reset
 end
 
 # element_exist(id: @contato,:timeout => 5)
 def element_exist(attribute)
-    begin
-        wait = Selenium::WebDriver::Wait.new :timeout => 5
-        wait.until { find_element(attribute).displayed? }
-    rescue 
+  begin
+    wait = Selenium::WebDriver::Wait.new
+    wait.until { find_element(attribute).displayed? }
+  rescue 
       return false
-    end
+  end
 end
