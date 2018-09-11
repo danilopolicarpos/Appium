eb874206a6f8826bc4f1d2347a758ffce05f2a23
Policarpo <danilo.policarpo@accenture.com>
2018-09-11 18:13:57 -0300

    rubocop

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 Before do
-  $driver.start_driver
+  driver.start_driver
 end
 
 After do
-  $driver.driver_quit
+  driver.driver_quit
 end
 
-Before ('@reinstall') do
+Before('@reinstall') do
   reinstall_apps
 end
 
-After ('@reinstall') do
+After('@reinstall') do
   @caps = Appium.load_appium_txt file: File.join(Dir.pwd, 'ios_caps.txt')
   Appium::Driver.new(@caps, true)
   Appium.promote_appium_methods Object
