b0230dd5454787cc8246f4efe84e6e8b9ffaa224
Policarpo <danilo.policarpo@accenture.com>
2018-09-12 10:29:38 -0300

    ajustes

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,9 +5,9 @@ require 'appium_lib'
 require 'pry'
 
 if ENV['PLATFORM'] == 'ios'
-  caps = Appium.load_appium_txt file: File.join('./../../../ios_appium.txt')
+  caps = Appium.load_appium_txt file: File.join('./ios_appium.txt')
 elsif ENV['PLATFORM'] == 'android'
-  caps = Appium.load_appium_txt file: File.join('./../../../android_appium.txt')
+  caps = Appium.load_appium_txt file: File.join('./android_appium.txt')
 end
 
 Appium::Driver.new(caps, true)
