5b13001848b163b4db70b5af5921a44801f08848
Policarpo <danilo.policarpo@accenture.com>
2018-10-24 17:05:45 -0300

    chat

---
+++ chat.js
@@ -0,0 +1,12 @@
+<script src="https://unpkg.com/blip-chat-widget" type="text/javascript">
+</script>
+    <script>
+        (function () {
+            window.onload = function () {
+                new BlipChat()
+                    .withAppKey('Y2V1OjYzNDYwNjY2LTgzYjAtNDA2Ny05YTUzLWEzYjljMTE1N2Y3NQ==')
+                    .withButton({ "color": "#2CC3D5" })
+                    .build();
+            }
+        })();
+</script>
